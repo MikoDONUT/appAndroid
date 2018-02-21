ff0c24b2ece1ce80ab2a326f8332bd0dacd3df5b
Matias Gomez <moicfritup@gmail.com>
2018-02-21 10:42:54 +0100

    def du Onclick

--- app/src/main/java/com/example/matias/pizza/PizzeriaMainActivity.java
+++ app/src/main/java/com/example/matias/pizza/PizzeriaMainActivity.java
@@ -16,42 +16,94 @@ public class PizzeriaMainActivity extends AppCompatActivity implements View.OnCl
     private Button button7;
     private Button button8;
 
+    private int click1=0;
+    private int click2=0;
+    private int click3=0;
+    private int click4=0;
+    private int click5=0;
+    private int click6=0;
+    private int click7=0;
+    private int click8=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pizzeria_main);
         button1 = (Button) findViewById(R.id.button1);
-        button1.setOnClickListener(this);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                click1++;
+                button1.setText("NAPOLITAINE " + ": " +click1 );
+            }
+        });
 
         button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(this);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                click2++;
+                button2.setText("ROYALE " + ": " +click2 );
+            }
+        });
 
         button3 = (Button) findViewById(R.id.button3);
-        button3.setOnClickListener(this);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                click3++;
+                button3.setText("QUATRE FROMAGES " + ": " +click3 );
+            }
+        });
 
         button4 = (Button) findViewById(R.id.button4);
-        button4.setOnClickListener(this);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                click4++;
+                button4.setText("MONTAGNARDE " + ": " +click4 );
+            }
+        });
 
         button5 = (Button) findViewById(R.id.button5);
-        button5.setOnClickListener(this);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                click5++;
+                button2.setText("RACLETTE " + ": " +click5 );
+            }
+        });
 
         button6 = (Button) findViewById(R.id.button6);
-        button6.setOnClickListener(this);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                click6++;
+                button6.setText("HAWAI " + ": " +click6 );
+            }
+        });
+
 
         button7 = (Button) findViewById(R.id.button7);
-        button7.setOnClickListener(this);
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                click7++;
+                button7.setText("PANNA COTA " + ": " +click7 );
+            }
+        });
 
         button8 = (Button) findViewById(R.id.button8);
-        button8.setOnClickListener(this);
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                click8++;
+                button8.setText("TIRAMISU " + ": " +click8 );
+            }
+        });
 
 
-    }
-
-    public void onClick(View v) {
-        if (v.getId() == R.id.button1) {
-    // action à définir
-        }
     }
 
 }
